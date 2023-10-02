e166c7494bc1926053c3ecaf645a47349553ec2f
Ismahan <main@ismahans-mbp.lan>
2023-10-02 15:53:15 -0400

    finished up UserService and added methods to UserController

--- src/main/java/com/bptn/feedApp/controller/UserController.java
+++ src/main/java/com/bptn/feedApp/controller/UserController.java
@@ -6,53 +6,52 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.bptn.feedApp.jdbc.UserBean;
+import com.bptn.feedApp.jpa.User;
 import com.bptn.feedApp.service.UserService;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 
-
 @RestController
 @RequestMapping("/user")
 public class UserController {
-	
+
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	UserService userService;
-	
+
 	@GetMapping("/")
-	public List<UserBean> listUsers() {
+	public List<User> listUsers() {
 		logger.debug("The listUsers() method was invoked!");
 		return this.userService.listUsers();
 	}
-	
-	
+
 	@GetMapping("/test")
 	public String testController() {
 		logger.debug("The testController() method was invoked!");
 		return "The FeedApp application is up and running and running and running";
-		
+
 	}
-	
+
 	@GetMapping("/{username}")
-	public UserBean findByUsername(@PathVariable String username) {
+	public Optional<User> findByUsername(@PathVariable String username) {
 		logger.debug("The findByUsername() method was invoked!, username={}", username);
 		return this.userService.findByUsername(username);
 	}
-	
-	
+
 	@GetMapping("/{first}/{last}/{username}/{password}/{phone}/{emailId}")
-	public String createUser( @PathVariable String first, @PathVariable String last, @PathVariable String username, @PathVariable String password, @PathVariable String phone, @PathVariable String emailId) {
-		UserBean user = new UserBean();
-		
+	public String createUser(@PathVariable String first, @PathVariable String last, @PathVariable String username,
+			@PathVariable String password, @PathVariable String phone, @PathVariable String emailId) {
+		User user = new User();
+
 		user.setFirstName(first);
 		user.setLastName(last);
 		user.setUsername(username);
@@ -61,13 +60,13 @@ public class UserController {
 		user.setEmailId(emailId);
 		user.setEmailVerified(false);
 		user.setCreatedOn(Timestamp.from(Instant.now()));
-				
+
 		logger.debug("The createUser() method was invoked!, user={}", user.toString());
-				
+
 		this.userService.createUser(user);
-				
+
 		return "User Created Successfully";
-		
-}
-	
+
+	}
+
 }
