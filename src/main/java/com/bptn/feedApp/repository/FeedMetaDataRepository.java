fa7b030fb3a4581fc61e6f8ae3737514008de8ce
Ismahan <main@ismahans-mbp.lan>
2023-10-03 13:21:18 -0400

    added FeedMetaData repository

---
+++ src/main/java/com/bptn/feedApp/repository/FeedMetaDataRepository.java
@@ -0,0 +1,9 @@
+package com.bptn.feedApp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.bptn.feedApp.jpa.FeedMetaData;
+
+public interface FeedMetaDataRepository extends JpaRepository<FeedMetaData, Integer> {
+}
