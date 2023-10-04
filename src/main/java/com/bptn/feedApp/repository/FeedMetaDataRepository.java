c0b2ea6083376c7b17eb4ac041915d712abee379
Ismahan <main@ismahans-mbp.lan>
2023-10-04 08:22:21 -0400

    finished up user sign up endpoint

--- src/main/java/com/bptn/feedApp/repository/FeedMetaDataRepository.java
+++ src/main/java/com/bptn/feedApp/repository/FeedMetaDataRepository.java
@@ -6,4 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bptn.feedApp.jpa.FeedMetaData;
 
 public interface FeedMetaDataRepository extends JpaRepository<FeedMetaData, Integer> {
+	
+	//methods, none yet because we have necessary ones.
+	
 }
