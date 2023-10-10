338dbfa2f18d2b97980d6efa09ed6f8682f09393
Ismahan <main@ismahans-mbp.lan>
2023-10-09 22:31:48 -0400

    finished WebSecurityConfig

--- src/main/java/com/bptn/feedApp/filter/CustomAuthEntryPoint.java
+++ src/main/java/com/bptn/feedApp/filter/CustomAuthEntryPoint.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
