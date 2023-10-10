87ab50a77ad6b455519e01ac661d91d928d6fe90
Ismahan <main@ismahans-mbp.lan>
2023-10-10 14:20:11 -0400

    fixed login credentials and end point

--- src/main/java/com/bptn/feedApp/controller/UserController.java
+++ src/main/java/com/bptn/feedApp/controller/UserController.java
@@ -4,18 +4,27 @@ import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.bptn.feedApp.jpa.User;
 import com.bptn.feedApp.service.UserService;
+import static org.springframework.http.HttpStatus.OK;
+import org.springframework.http.HttpHeaders;
 
+
+
+@CrossOrigin(exposedHeaders = "Authorization")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -70,6 +79,22 @@ public class UserController {
 		logger.debug("Verifying Email");
 			
 		this.userService.verifyEmail();
+	}
+	
+	@PostMapping("/login")
+	public ResponseEntity<User> login(@RequestBody User user) {
+		
+		logger.debug("Authenticating, username: {}, password: {}", user.getUsername(), user.getPassword());
+			
+		/* Spring Security Authentication. */
+		user = this.userService.authenticate(user);
+
+		/* Generate JWT and HTTP Header */
+		HttpHeaders jwtHeader = this.userService.generateJwtHeader(user.getUsername());
+					
+		logger.debug("User Authenticated, username: {}", user.getUsername());
+			
+		return new ResponseEntity<>(user, jwtHeader, OK);
 	}
 	
 	
