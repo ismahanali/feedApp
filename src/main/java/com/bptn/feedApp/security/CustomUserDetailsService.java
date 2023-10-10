07172dd36208937875462066bb280a4cee6f7291
Ismahan <main@ismahans-mbp.lan>
2023-10-09 21:09:26 -0400

    CustomUserDetailsService creation

---
+++ src/main/java/com/bptn/feedApp/security/CustomUserDetailsService.java
@@ -0,0 +1,69 @@
+package com.bptn.feedApp.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.bptn.feedApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bptn.feedApp.jpa.User;
+import java.util.Optional;
+
+@Service("UserDetailsService")
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		Optional<User> opt = this.userRepository.findByUsername(username);
+
+		if (opt.isEmpty()) {
+			throw new UsernameNotFoundException("Username not found: " + username);
+		}
+
+		return new CustomUserDetails(opt.get());
+	}
+
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
