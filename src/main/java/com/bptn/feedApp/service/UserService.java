9d5e2a3a212772d1c11459bb1298d8cc2b84228b
Ismahan <main@ismahans-mbp.lan>
2023-10-11 11:40:23 -0400

    adding update user

--- src/main/java/com/bptn/feedApp/service/UserService.java
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -1,17 +1,25 @@
 package com.bptn.feedApp.service;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
 import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.bptn.feedApp.exception.domain.EmailExistException;
 import com.bptn.feedApp.exception.domain.EmailNotVerifiedException;
@@ -21,15 +29,10 @@ import com.bptn.feedApp.jpa.User;
 import com.bptn.feedApp.provider.ResourceProvider;
 import com.bptn.feedApp.repository.UserRepository;
 import com.bptn.feedApp.security.JwtService;
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import org.springframework.http.HttpHeaders;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Service
 public class UserService {
-	
+
 	final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
@@ -40,16 +43,15 @@ public class UserService {
 
 	@Autowired
 	PasswordEncoder passwordEncoder;
-	
+
 	@Autowired
 	JwtService jwtService;
 
 	@Autowired
 	ResourceProvider provider;
-	
+
 	@Autowired
 	AuthenticationManager authenticationManager;
-
 
 	public List<User> listUsers() {
 		return this.userRepository.findAll();
@@ -104,18 +106,20 @@ public class UserService {
 
 		this.userRepository.save(user);
 	}
+
 	private static User isEmailVerified(User user) {
-		 
+
 		if (user.getEmailVerified().equals(false)) {
-	        throw new EmailNotVerifiedException(String.format("Email requires verification, %s", user.getEmailId()));
-	    }	
-			
-	    return user;
+			throw new EmailNotVerifiedException(String.format("Email requires verification, %s", user.getEmailId()));
+		}
+
+		return user;
 	}
-	
+
 	private Authentication authenticate(String username, String password) {
 		return this.authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-}
+	}
+
 	public User authenticate(User user) {
 
 		/* Spring Security Authentication. */
@@ -124,13 +128,14 @@ public class UserService {
 		/* Get User from the DB. */
 		return this.userRepository.findByUsername(user.getUsername()).map(UserService::isEmailVerified).get();
 	}
+
 	public HttpHeaders generateJwtHeader(String username) {
 		HttpHeaders headers = new HttpHeaders();
-		headers.add(AUTHORIZATION, this.jwtService.generateJwtToken(username,this.provider.getJwtExpiration()));
+		headers.add(AUTHORIZATION, this.jwtService.generateJwtToken(username, this.provider.getJwtExpiration()));
 
 		return headers;
 	}
-	
+
 	public void sendResetPasswordEmail(String emailId) {
 
 		Optional<User> opt = this.userRepository.findByEmailId(emailId);
@@ -141,24 +146,64 @@ public class UserService {
 			logger.debug("Email doesn't exist, {}", emailId);
 		}
 	}
+
 	public void resetPassword(String password) {
 
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
 		User user = this.userRepository.findByUsername(username)
-					.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s",username)));
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
 
 		user.setPassword(this.passwordEncoder.encode(password));
 
 		this.userRepository.save(user);
 	}
+
 	public User getUser() {
 
 		String username = SecurityContextHolder.getContext().getAuthentication().getName();
 
 		/* Get User from the DB. */
 		return this.userRepository.findByUsername(username)
-	.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s",username)));
+				.orElseThrow(() -> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
 	}
-	
+
+	private void updateValue(Supplier<String> getter, Consumer<String> setter) {
+
+		Optional.ofNullable(getter.get())
+				// .filter(StringUtils::hasText)
+				.map(String::trim).ifPresent(setter);
+	}
+
+	private void updatePassword(Supplier<String> getter, Consumer<String> setter) {
+
+		Optional.ofNullable(getter.get()).filter(StringUtils::hasText).map(this.passwordEncoder::encode)
+				.ifPresent(setter);
+	}
+
+	private User updateUser(User user, User currentUser) {
+
+		this.updateValue(user::getFirstName, currentUser::setFirstName);
+		this.updateValue(user::getLastName, currentUser::setLastName);
+		this.updateValue(user::getPhone, currentUser::setPhone);
+		this.updateValue(user::getEmailId, currentUser::setEmailId);
+		this.updatePassword(user::getPassword, currentUser::setPassword);
+
+		return this.userRepository.save(currentUser);
+	}
+	public User updateUser(User user) {
+		
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		/* Validates the new email if provided */
+		this.userRepository.findByEmailId(user.getEmailId())
+	                            .filter(u->!u.getUsername().equals(username))
+	                            .ifPresent(u -> {throw new EmailExistException(String.format("Email already exists, %s", u.getEmailId()));});
+		    
+		/* Get and Update User */	
+		return this.userRepository.findByUsername(username)
+					            .map(currentUser -> this.updateUser(user, currentUser))
+					            .orElseThrow(()-> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+	}
+
 }
