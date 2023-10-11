a8b90a2f14e04f1217dff4825fda23b214e5ca1a
Ismahan <main@ismahans-mbp.lan>
2023-10-11 14:21:08 -0400

    added update profile method to controller and UserService

--- src/main/java/com/bptn/feedApp/service/UserService.java
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -29,6 +29,7 @@ import com.bptn.feedApp.jpa.User;
 import com.bptn.feedApp.provider.ResourceProvider;
 import com.bptn.feedApp.repository.UserRepository;
 import com.bptn.feedApp.security.JwtService;
+import com.bptn.feedApp.jpa.Profile;
 
 @Service
 public class UserService {
@@ -204,6 +205,35 @@ public class UserService {
 		return this.userRepository.findByUsername(username)
 					            .map(currentUser -> this.updateUser(user, currentUser))
 					            .orElseThrow(()-> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+	}
+	
+	private User updateUserProfile(Profile profile, User user) {
+
+		Profile currentProfile = user.getProfile();
+
+		if (Optional.ofNullable(currentProfile).isPresent()) {
+
+			this.updateValue(profile::getHeadline, currentProfile::setHeadline);
+			this.updateValue(profile::getBio, currentProfile::setBio);
+			this.updateValue(profile::getCity, currentProfile::setCity);
+			this.updateValue(profile::getCountry, currentProfile::setCountry);
+			this.updateValue(profile::getPicture, currentProfile::setPicture);
+		} 
+	    else {
+			user.setProfile(profile);
+			profile.setUser(user);
+		}
+
+		return this.userRepository.save(user);
+	}
+	public User updateUserProfile(Profile profile) {
+		
+		String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+		/* Get and Update User */	
+		return this.userRepository.findByUsername(username)
+		              .map(user -> this.updateUserProfile(profile, user))
+	                  .orElseThrow(()-> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
 	}
 
 }
